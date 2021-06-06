3863c1b441ed0d391cc412d07f39e6a9f47fb47d
Tommy P. Keane <talk@tommypkeane.com>
2021-06-06 18:10:56 -0400

    [CLN] Ran eslint to fix code
    - Ran `eslint --fix .` to fix the code per the configuration file(s)

--- tommypkeane_com/src/App.js
+++ tommypkeane_com/src/App.js
@@ -1,6 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+/**
+ * Runtime Application Function
+ * @function
+ */
 function App() {
   return (
     <div className="App">
